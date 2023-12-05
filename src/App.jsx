5d2b00f998949b9336fcb105160966b28beaee50
Sergey <sergsmile29@gmail.com>
2023-12-05 10:15:09 +0200

    uncontrolled form

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import './styles.css'
-import { TodoList } from './components/TodoList/TodoList'
 import { Employee } from './components/Employees/Employee'
 import { RegisterForm } from './components/RegisterForm/RegisterForm'
 import { RegisterFormUncontrolled } from './components/RegisterForm/RegisterFormUncontrolled'
@@ -8,8 +7,8 @@ import { RegisterFormUncontrolled } from './components/RegisterForm/RegisterForm
 export const App = () => {
 	return (
 		<>
-			<Employee />
-			<RegisterForm />
+			{/* <Employee /> */}
+			{/* <RegisterForm /> */}
 			<RegisterFormUncontrolled />
 		</>
 	)
--- src/components/RegisterForm/RegisterFormUncontrolled.jsx
+++ src/components/RegisterForm/RegisterFormUncontrolled.jsx
@@ -2,23 +2,37 @@ import { FlexContainer, StyledButton } from '../Counter/Counter.styled'
 import { StyledForm, StyledInput, StyledLabel, StyledTitle } from './RegisterForm.styled'
 
 export const RegisterFormUncontrolled = () => {
+	const handleSubmit = event => {
+		event.preventDefault()
+		const form = event.target
+		const name = form.elements.name.value
+		const email = form.elements.email.value
+		const password = form.elements.password.value
+		const gender = form.elements.gender.value
+		const country = form.elements.country.value
+		const formData = new FormData()
+		formData.append('name', name)
+		formData.append('email', email)
+		console.log({ name, email, password, gender, country })
+		form.reset()
+	}
 	return (
 		<FlexContainer>
-			<StyledForm>
+			<StyledForm onSubmit={handleSubmit}>
 				<StyledTitle>Register</StyledTitle>
 				<StyledLabel>
 					Name:
-					<StyledInput />
+					<StyledInput name='name' />
 				</StyledLabel>
 				<br />
 				<StyledLabel>
 					Email:
-					<StyledInput />
+					<StyledInput name='email' />
 				</StyledLabel>
 				<br />
 				<StyledLabel>
 					Password:
-					<StyledInput type='password' />
+					<StyledInput name='password' type='password' />
 				</StyledLabel>
 				<br />
 				<StyledLabel>Gender:</StyledLabel>
@@ -28,7 +42,7 @@ export const RegisterFormUncontrolled = () => {
 				<br />
 				<StyledLabel>
 					Country:
-					<select>
+					<select name='country'>
 						<option value='ukraine'>Ukraine</option>
 						<option value='usa'>USA</option>
 						<option value='canada'>Canada</option>
