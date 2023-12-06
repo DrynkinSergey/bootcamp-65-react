3c1899db956cd4ff7f20d3cedca64adb9cf453df
Sergey <sergsmile29@gmail.com>
2023-12-07 00:03:59 +0200

    init m3l2

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,5 @@
-import React from 'react'
 import './styles.css'
-import { Employee } from './components/Employees/Employee'
-import { RegisterForm } from './components/RegisterForm/RegisterForm'
-import { RegisterFormUncontrolled } from './components/RegisterForm/RegisterFormUncontrolled'
-import Modal from './components/Modal/Modal'
-import { Counter } from './components/Counter/Counter'
-import { ColorPicker } from './components/ColorPicker/ColorPicker'
-import colors from './assets/colors.json'
 
 export const App = () => {
-	return (
-		<>
-			{/* <Counter /> */}
-			{/* <Employee /> */}
-			<ColorPicker colors={colors} />
-			{/* <RegisterForm /> */}
-			{/* <RegisterFormUncontrolled /> */}
-		</>
-	)
+	return <></>
 }
