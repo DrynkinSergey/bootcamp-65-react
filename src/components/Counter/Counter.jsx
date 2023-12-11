b371a973bbff5eefafeba58e9cb3a4e592e11693
Sergey <sergsmile29@gmail.com>
2023-12-11 10:33:39 +0200

    useState counter

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,62 +1,105 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FlexContainer, StyledCounter } from './Counter.styled'
 import { Buttons } from './Buttons'
 
-export class Counter extends React.Component {
-	state = {
-		counter: 0,
-		step: 1,
-	}
-	componentDidMount() {
-		console.log('Mount - Монтування пройшло!')
+export const Counter = () => {
+	const [counter, setCounter] = useState(0)
+	const [step, setStep] = useState(1)
+
+	const handleIncrement = () => {
+		// this.setState(prevState => ({ counter: prevState.counter + prevState.step }))
+
+		setCounter(prevState => prevState + step)
 	}
 
-	componentDidUpdate(_, prevState) {
-		if (prevState.counter !== this.state.counter) {
-			console.log('Update - Оновлення компонента через лічильник!')
-		}
-		if (prevState.step !== this.state.step) {
-			console.log('Step змінився!')
-		}
-		if (this.state.counter === 5) {
-			console.log('Зупинись ми вже = 5')
-		}
-		if (this.state.counter === 10) {
-			this.setState({ counter: 0 })
-		}
+	const handleDecrement = () => {
+		// this.setState(prevState => ({ counter: prevState.counter - prevState.step }))
+
+		setCounter(prevState => prevState - step)
 	}
 
-	handleIncrement = () => {
-		this.setState(prevState => ({ counter: prevState.counter + prevState.step }))
+	const handleReset = () => {
+		// this.setState({ counter: 0, step: 1 })
+		setCounter(0)
+		setStep(1)
 	}
 
-	handleDecrement = () => {
-		this.setState(prevState => ({ counter: prevState.counter - prevState.step }))
+	const handleChangeStep = e => {
+		// this.setState({ step: +e.target.value })
+		setStep(+e.target.value)
 	}
 
-	handleReset = () => {
-		this.setState({ counter: 0, step: 1 })
-	}
-
-	handleChangeStep = e => {
-		this.setState({ step: +e.target.value })
-	}
-
-	render() {
-		const { counter, step } = this.state
-		return (
-			<FlexContainer>
-				<StyledCounter>
-					<h1>{counter}</h1>
-					<input type='text' value={step} onChange={this.handleChangeStep} />
-					<Buttons
-						counter={counter}
-						handleDecrement={this.handleDecrement}
-						handleIncrement={this.handleIncrement}
-						handleReset={this.handleReset}
-					/>
-				</StyledCounter>
-			</FlexContainer>
-		)
-	}
+	return (
+		<FlexContainer>
+			<StyledCounter>
+				<h1>{counter}</h1>
+				<input type='text' value={step} onChange={handleChangeStep} />
+				<Buttons
+					counter={counter}
+					handleDecrement={handleDecrement}
+					handleIncrement={handleIncrement}
+					handleReset={handleReset}
+				/>
+			</StyledCounter>
+		</FlexContainer>
+	)
 }
+
+// export class Counter extends React.Component {
+// 	state = {
+// 		counter: 0,
+// 		step: 1,
+// 	}
+// 	componentDidMount() {
+// 		console.log('Mount - Монтування пройшло!')
+// 	}
+
+// 	componentDidUpdate(_, prevState) {
+// 		if (prevState.counter !== this.state.counter) {
+// 			console.log('Update - Оновлення компонента через лічильник!')
+// 		}
+// 		if (prevState.step !== this.state.step) {
+// 			console.log('Step змінився!')
+// 		}
+// 		if (this.state.counter === 5) {
+// 			console.log('Зупинись ми вже = 5')
+// 		}
+// 		if (this.state.counter === 10) {
+// 			this.setState({ counter: 0 })
+// 		}
+// 	}
+
+// 	handleIncrement = () => {
+// 		this.setState(prevState => ({ counter: prevState.counter + prevState.step }))
+// 	}
+
+// 	handleDecrement = () => {
+// 		this.setState(prevState => ({ counter: prevState.counter - prevState.step }))
+// 	}
+
+// 	handleReset = () => {
+// 		this.setState({ counter: 0, step: 1 })
+// 	}
+
+// 	handleChangeStep = e => {
+// 		this.setState({ step: +e.target.value })
+// 	}
+
+// 	render() {
+// 		const { counter, step } = this.state
+// return (
+// 	<FlexContainer>
+// 		<StyledCounter>
+// 			<h1>{counter}</h1>
+// 			<input type='text' value={step} onChange={this.handleChangeStep} />
+// 			<Buttons
+// 				counter={counter}
+// 				handleDecrement={this.handleDecrement}
+// 				handleIncrement={this.handleIncrement}
+// 				handleReset={this.handleReset}
+// 			/>
+// 		</StyledCounter>
+// 	</FlexContainer>
+// )
+// 	}
+// }
