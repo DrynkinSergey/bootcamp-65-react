f1b285b7da514cca63459df724b1b5180b841b8e
Sergey <sergsmile29@gmail.com>
2023-12-12 12:46:49 +0200

    postsReducer

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,21 +1,16 @@
-import React, { useCallback, useContext, useEffect, useState } from 'react'
+import React, { useCallback, useContext, useEffect, useReducer, useState } from 'react'
 import SearchForm from './SearchForm'
 import { PostList } from './PostList'
 import styled from 'styled-components'
 import { fetchPosts, fetchPostsByQuery } from '../../services/api'
 import { Comment } from 'react-loader-spinner'
 import { UserContext } from '../../context/ContextProvider'
+import { initialState, postReducer } from '../../reducer/postReducer'
 
 export const Posts = () => {
-	const [posts, setPosts] = useState([])
-	const [loading, setLoading] = useState(false)
-	const [error, setError] = useState(null)
-	const [skip, setSkip] = useState(0)
-	const [searchQuery, setSearchQuery] = useState('')
-	const [totalPosts, setTotalPosts] = useState(null)
-
 	const { user, logout } = useContext(UserContext)
-
+	const [state, dispatch] = useReducer(postReducer, initialState)
+	const { posts, loading, error, skip, searchQuery, totalPosts } = state
 	// xd01r10
 	// r010dx
 	// r001xr
@@ -23,8 +18,9 @@ export const Posts = () => {
 
 	const getData = useCallback(async () => {
 		try {
-			setLoading(true)
-			setError(null)
+			dispatch({ type: 'SET_LOADING', payload: true })
+			// setLoading(true)
+			// setError(null)
 
 			//Перевіряється чи є у нас строка пошуку
 			const { posts, total } = searchQuery
@@ -32,13 +28,15 @@ export const Posts = () => {
 				  await fetchPostsByQuery({ skip, q: searchQuery })
 				: // Якщо нема - зробити запит без ключового слова
 				  await fetchPosts({ skip })
-
-			setPosts(prev => [...prev, ...posts])
-			setTotalPosts(total)
+			dispatch({ type: 'FETCH_DATA', payload: { posts, total } })
+			// setPosts(prev => [...prev, ...posts])
+			// setTotalPosts(total)
 		} catch (error) {
-			setError(error.message)
+			// setError(error.message)
+			dispatch({ type: 'SET_ERROR', payload: error.message })
 		} finally {
-			setLoading(false)
+			// setLoading(false)
+			dispatch({ type: 'SET_LOADING', payload: false })
 		}
 	}, [skip, searchQuery])
 
@@ -47,12 +45,14 @@ export const Posts = () => {
 	}, [searchQuery, skip, getData])
 
 	const handleLoadMore = () => {
-		setSkip(prev => prev + 4)
+		// setSkip(prev => prev + 4)
+		dispatch({ type: 'SET_SKIP' })
 	}
 	const handleSetSearchQuery = text => {
-		setSearchQuery(text)
-		setPosts([])
-		setSkip(0)
+		dispatch({ type: 'CHANGE_QUERY', payload: text })
+		// setSearchQuery(text)
+		// setPosts([])
+		// setSkip(0)
 	}
 
 	return (
