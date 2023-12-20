f56be79cad8ab089615579907db5a8e568070e5f
Sergey <sergsmile29@gmail.com>
2023-12-20 12:40:12 +0200

    add custom middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,8 +7,17 @@ const rootReducer = combineReducers({
 	todoData: todoReducer,
 })
 
+const myMiddleware = store => next => action => {
+	console.log(action)
+	if (action?.payload?.title === 'angular') {
+		console.log('ALERT ANGULAR IS NOT AVAILABLE')
+		action.payload.title = 'react'
+	}
+	next(action)
+}
+
 export const store = configureStore({
 	reducer: rootReducer,
-	middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger),
+	middleware: getDefaultMiddleware => getDefaultMiddleware().concat(myMiddleware),
 	devTools: process.env.NODE_ENV !== 'production',
 })
