fd181cb288b98cd0ee51dea8f67256fa43223621
Sergey <sergsmile29@gmail.com>
2023-12-21 00:26:36 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { TodoList } from './components/TodoList/TodoList'
+import { Articles } from './components/Articles/Articles'
 
 export const App = () => {
 	return (
 		<div>
 			<TodoList />
+			<Articles />
 		</div>
 	)
 }
---
+++ src/components/Articles/Articles.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const Articles = () => {
+	return <div>Articles</div>
+}
